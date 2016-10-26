09d1cc19ff7f8b2c78d70e6743510a89b0f67c21
Recca Tsai <recca0120.gmail.com>
2016-10-26 18:58:08 +0800

    結果還是不行，我們來增加user agent試看看

--- test.php
+++ test.php
@@ -3,11 +3,20 @@
 use Recca0120\TaiwanBank\Gold;
 use Http\Adapter\Guzzle6\Client;
 use Http\Message\MessageFactory\GuzzleMessageFactory;
+use Http\Message\CookieJar;
+use Http\Client\Common\PluginClient;
+use Http\Client\Common\Plugin\CookiePlugin;
 
 require __DIR__.'/vendor/autoload.php';
 
 $client = new Client();
 $messageFactory = new GuzzleMessageFactory();
 
-$gold = new Gold($client, $messageFactory);
+$cookiePlugin = new CookiePlugin(new CookieJar());
+$pluginClient = new PluginClient(
+    $client,
+    [$cookiePlugin]
+);
+
+$gold = new Gold($pluginClient, $messageFactory);
 echo $gold->getHtml();
