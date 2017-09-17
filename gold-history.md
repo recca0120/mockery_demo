4306072240fff648efb6cd55a789a25607f76a12
recca0120 <recca0120@gmail.com>
2017-09-18 01:49:26 +0800

    補上 Client.php 的測試

--- gold-history.md
+++ gold-history.md
@@ -29,4 +29,5 @@ composer dump-autoload
 - 準備好測試案例，並使測試亮第一個紅燈
 - 完成第一個測試
 - 現在我們不希望一直抓取網頁，所以我們開始第一次重構
-- 重構到 Client.php 後，並補上 Client.php 的測試
+- 將抓取網頁的程式重構到 Client.php
+- 補上 Client.php 的測試
---
+++ tests/ClientTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Acme\Tests;
+
+use Acme\Client;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function test_get_html() 
+    {
+        $client = new Client;
+ 
+        $this->assertSame(file_get_contents(__DIR__.'/gold-history.html'), $client->get());
+    }
+} 
